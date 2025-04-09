645f6153af6708c6c9e8a6cd5c72e506fd2f9f9a
MonaHanna95 <mona.hannam@gmail.com>
2025-04-09 12:07:16 +0300

    Add files via upload

--- pages/1_About.py
+++ pages/1_About.py
@@ -1,1 +1,13 @@
+import streamlit as st
 
+st.title("About BE Water Stand")
+st.write("""
+**BE Water Stand** was created with a simple idea: make clean water accessible to everyone, while educating people about sustainability.
+
+Each dispenser:
+- Offers filtered, quality-tested water
+- Displays environmental awareness content on a large screen
+- Is powered by sponsorship from NGOs and companies’ CSR programs
+
+Our goal is to create a **network of water access points** while encouraging eco-friendly choices.
+""")
